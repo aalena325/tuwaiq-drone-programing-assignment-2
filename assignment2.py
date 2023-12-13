42f33cd4af4b5331ba8c4d6b7c1dd389a46a6af8
Wajd <152892800+Wmso@users.noreply.github.com>
2023-12-13 22:44:39 +0300

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+from djitellopy import tello
+from cvzone.PoseModule import PoseDetector
+import cv2
+
+
+detector = PoseDetector()
+cap = cv2.VideoCapture(0)
+drone = tello.Tello()
+
+drone.connect()
+drone.takeoff()
+drone.move_up(60)
+drone.streamon()
+
+for i in range(4):
+    img = drone.get_frame_read().frame
+    img = detector.findPose(img, draw=True)
+    
+    cv2.imshow("image", img)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+    drone.rotate_clockwise(45)
+
+drone.land
